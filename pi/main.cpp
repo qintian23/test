853e1a5a911bdbad233fa855c76dafb5e04b398f
Liucheng Zhu <57068431+qintian23@users.noreply.github.com>
2020-06-16 23:31:01 +0800

    Update main.cpp

--- pi/main.cpp
+++ pi/main.cpp
@@ -1,7 +1,7 @@
 /*
-	Բ���ʵ�ͳ��Ԥ�⣬���Ȱ���֤÷���๫ʽ��׼ȷ�Է�װΪһ��ͷ�ļ�.
-	Ȼ��,���򵥵�ͳ�Ʒ���,���MATLAB.
-											ZLC,2020/6/14
+	圆周率的统计预测，首先把验证梅钦类公式的准确性封装为一个头文件.
+	然后,做简单的统计分析,结合MATLAB.
+											ZLC,2020/6/16
 */
 
 #include <iostream>
@@ -12,18 +12,18 @@
 using namespace std;
 
 int main(void) {
-	int data[N]; // ����Ԫ��
-	int frequency[10]; // Ƶ��ͳ��Ԫ��
-	// ʹ��÷���๫ʽ����PI��ǰ800λ����ε�������ǰ1000λ��PIֵ��
+	int data[N]; // 数据元组
+	int frequency[10]; // 频度统计元组
+	// 使用梅钦类公式生成PI的前800位（如何调参生成前1000位的PI值）
 	Machin();
-	// ��ȡ��ʵPIֵ��ǰ1000λ
+	// 读取真实PI值的前1000位
 	readTxt("realPI.txt");
-	// ��֤÷���๫ʽ��piֵ����ȷ��
-	cout<< "÷���๫ʽ��piֵ��׼ȷ��: " << rate(comparestr()) << "%" << endl;
+	// 验证梅钦类公式的pi值的正确性
+	cout<< "梅钦类公式的pi值的准确率: " << rate(comparestr()) << "%" << endl;
 
-	/* ͳ�Ʒ��� */
-	cout << "********************************************** ͳ�Ʒ��� **********************************************" << endl;
-	// 1.����Ԥ����
+	/* 统计分析 */
+	cout << "********************************************** 统计分析 **********************************************" << endl;
+	// 1.数据预处理
 	preprocessing(data);
 	/*
 	for (int i = 0; i < N; i++) {
@@ -31,15 +31,15 @@ int main(void) {
 	}
 	cout<< endl;
 	*/
-	// 2.����Ƶ��ͳ��
-	memset(frequency,0,sizeof(frequency)); // ��ʼ��,�õ�һ��ȫ���Ƶ��ͳ��Ԫ��
+	// 2.数字频度统计
+	memset(frequency,0,sizeof(frequency)); // 初始化,得到一个全零的频度统计元组
 	for (int i = 0; i < N; i++) {
 		frequency[data[i]] ++;
 	}
 	for (int j = 0; j < 10; j++) {
 		cout << j << "\t" << frequency[j] << endl;
 	}
-	// 3.����Ƶ��ͼ��
+	// 3.画出频度图像
 
 	system("pause");
 
